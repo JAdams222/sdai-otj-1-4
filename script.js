2931c2656d9a6fbfcb66d3dafea035a114301488
Joshua Adams <122932021+JAdams222@users.noreply.github.com>
2024-08-27 03:16:43 +0000

    changes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Ensure the DOM is fully loaded before executing scripts
 document.addEventListener('DOMContentLoaded', function() {
-    // Image Carousel Functionality
+    
     const galleryImages = document.querySelectorAll('.dev-favorites-gallery img');
     let currentImageIndex = 0;
 
-    // Function to show the current image and hide others
+    
     function showImage(index) {
         galleryImages.forEach((img, i) => {
             img.style.display = i === index ? 'block' : 'none';
